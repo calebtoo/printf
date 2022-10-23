0fa2e41d42b531b7cdbfdcd32a49b6f30f57b87e
Billy Munene <billymunenen62@gmail.com>
2022-10-23 16:13:15 -0700

    A few printfs again

--- main.h
+++ main.h
@@ -4,31 +4,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
 
 /**
- * struct fmt - function to check for formats
- * @type: format to print
+ * struct op - function to check for formats
+ * @c: format to print
  * @f: print function to use
  */
 
-typedef struct fmt
+typedef struct op
 {
-	char *type;
-	int (*f)();
-} fmt_t;
+	char *c;
+	int (*f)(va_list);
+} op_t;
 
 int _printf(const char *format, ...);
-int print_op(const char *format, fmt_t *print_arr, va_list list);
-int ch(va_list character);
-int str(va_list string);
-int _int(va_list integ);
-int _ui(va_list unsign);
-int _oct(va_list octo);
-int _rot13(va_list rot);
-int _hex_str(unsigned int n, unsigned int hex, char alpha);
-int _hex_l(va_list hexa);
-int _hex_u(va_list hexa);
-int _strlen(char *s);
-int _bin(va_list bin);
 int _putchar(char c);
+int print_c(va_list ar_list);
+int (*get_func(char s))(va_list ar_list);
+int print_s(va_list ar_list);
+int print_d(va_list ar_list);
+int print_i(va_list ar_numlist);
+int print_b(va_list binary_list);
+int print_u(va_list ar_list);
+int print_o(va_list ar_list);
+int print_x(va_list ar_list);
+int print_X(va_list ar_list);
+
 #endif
